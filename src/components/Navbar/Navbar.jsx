047fb1982ce0f75853ff8a1b0a2441867de9fbc1
Shawn <shawn17@live.fr>
2021-09-15 10:17:04 +0200

    reforging Navbar component, adding proper logo, and fixing the static style of the desktop version

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,31 @@
+import './Navbar.css'
+import logo from '../../assets/greeters-logo-red.PNG'
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
-  return <div className='Navbar'></div>
+  return (
+    <div className='Navbar'>
+      <div className='nav-element'>
+        <span className='nav-menu-button'>&#9776;</span>
+      </div>
+
+      <div className='nav-element logo-element'>
+        <NavLink to='/'>
+          <img src={logo} alt='logo' />
+        </NavLink>
+      </div>
+
+      <div className='nav-element'>
+        <div className='nav-sub-element'>
+          <button>&#128270;</button>
+          <select>
+            <option>FR</option>
+            <option>EN</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
