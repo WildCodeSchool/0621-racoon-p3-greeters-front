85e5baf1918ae44a8fd3a531101083980038ae65
Shawn <shawn17@live.fr>
2021-09-16 17:39:36 +0200

    adding leaflet map and fixing the config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,109 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import './App.css'
+// import Navbar from './components/Navbar/Navbar'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import Map from './components/Map/Map'
 
-function App() {
-  return (
-    <div className='App'>
-      <BrowserRouter>
-        <Route path='/' exact>
-          <Navbar />
-        </Route>
-      </BrowserRouter>
-    </div>
-  )
+// function App() {
+//   return (
+//     <div className='App'>
+//       <BrowserRouter>
+//         <Navbar />
+//         <Map />
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { Component } from 'react'
+import './App.css'
+import L from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import leafGreen from './assets/map/leaf-green.png'
+import leafRed from './assets/map/leaf-red.png'
+import leafOrange from './assets/map/leaf-orange.png'
+import leafShadow from './assets/map/leaf-shadow.png'
+
+class App extends Component {
+  state = {
+    greenIcon: {
+      lat: 35.787449,
+      lng: -78.6438197
+    },
+    redIcon: {
+      lat: 35.774416,
+      lng: -78.633271
+    },
+    orangeIcon: {
+      lat: 35.77279,
+      lng: -78.652305
+    },
+    zoom: 13
+  }
+
+  grenIcon = L.icon({
+    iconUrl: leafGreen,
+    shadowUrl: leafShadow,
+    iconSize: [38, 95], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -76]
+  })
+
+  redIcon = L.icon({
+    iconUrl: leafRed,
+    shadowUrl: leafShadow,
+    iconSize: [38, 95], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -86]
+  })
+
+  orangeIcon = L.icon({
+    iconUrl: leafOrange,
+    shadowUrl: leafShadow,
+    iconSize: [38, 95], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -86]
+  })
+
+  render() {
+    const positionRedIcon = [this.state.redIcon.lat, this.state.redIcon.lng]
+    const positionGreenIcon = [
+      this.state.greenIcon.lat,
+      this.state.greenIcon.lng
+    ]
+    const positionOrangeIcon = [
+      this.state.orangeIcon.lat,
+      this.state.orangeIcon.lng
+    ]
+    return (
+      <MapContainer
+        className='map'
+        center={positionGreenIcon}
+        zoom={this.state.zoom}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={positionGreenIcon} icon={this.grenIcon}>
+          <Popup>I am a green leaf</Popup>
+        </Marker>
+        <Marker position={positionRedIcon} icon={this.redIcon}>
+          <Popup>I am a red leaf</Popup>
+        </Marker>
+        <Marker position={positionOrangeIcon} icon={this.orangeIcon}>
+          <Popup>I am an orange leaf</Popup>
+        </Marker>
+      </MapContainer>
+    )
+  }
 }
 
 export default App
