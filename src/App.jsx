d680c6bee4b238614dcdb2189a19d86d051426aa
Remy Chazelas <remychazelas@gmail.com>
2021-09-17 11:59:21 +0200

    add style to search & nav bar logo center

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
-import CitySearch from './assets/Data/CitySearch'
 
 function App() {
   return (
@@ -9,11 +8,6 @@ function App() {
       <BrowserRouter>
         <Route path='/' exact>
           <Navbar />
-          <div className='city'>
-            {CitySearch.map(item => (
-              <h1 key={item.id}>{item.name}</h1>
-            ))}
-          </div>
         </Route>
       </BrowserRouter>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,6 +22,8 @@ const Navbar = () => {
   const handleSearchBar = () => {
     setSearchBar(!searchBar)
   }
+  /***** Search value */
+  const [searchValue, setSearchValue] = useState('')
 
   /***** Menu */
   const [showMenu, setShowMenu] = useState(false)
@@ -34,6 +36,11 @@ const Navbar = () => {
     <div className='Navbar'>
       {/* Fixed Menu */}
       <div className='nav-close'>
+        <div className='logo-element'>
+          <NavLink to='/'>
+            <img src={logo} alt='logo' />
+          </NavLink>
+        </div>
         <div className='nav-element'>
           <div
             className={
@@ -48,16 +55,16 @@ const Navbar = () => {
             <span></span>
           </div>
         </div>
-        <div className='nav-element logo-element'>
-          <NavLink to='/'>
-            <img src={logo} alt='logo' />
-          </NavLink>
-        </div>
-        {/* searchBar */}
-        <SearchBar />
-        {/* searchBar */}
         <div className='nav-element'>
           <div className='nav-sub-element'>
+            {/* searchBar */}
+            {searchBar && (
+              <SearchBar
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+              />
+            )}
+
             <button onClick={handleSearchBar}>&#128270;</button>
             <select
               className='language-button'
