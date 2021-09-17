62dec512a46f0e844a508564cfcb7dfc921696d6
BenoitGaudin <83647592+BenoitGaudin@users.noreply.github.com>
2021-09-17 12:20:57 +0200

    create footer folder

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className="App">
+          <h1>Footer HOURS</h1>
+        </div>
+    )
+}
+
+export default Footer;
