fbb71a134d8e1e768075a5997393192e045b1b9c
Shawn <shawn17@live.fr>
2021-09-21 17:22:06 +0200

    changing the structure, adding fields, changing style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,17 +5,37 @@ import Swal from 'sweetalert2'
 import './Contact.css'
 
 const Contact = () => {
+  const [object, setObject] = useState('')
   const [message, setMessage] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
 
   const [fullMessage, setFullMessage] = useState({})
+
+  const handleObject = object => {
+    setObject(object)
+  }
 
   const handleMessage = message => {
     setMessage(message)
   }
 
+  const handleLastName = lastName => {
+    setLastName(lastName)
+  }
+
+  const handleFirstName = firstName => {
+    setFirstName(firstName)
+  }
+
   const handleEmail = email => {
     setEmail(email)
+  }
+
+  const handlePhone = phone => {
+    setPhone(phone)
   }
 
   const handleSubmit = (e, message, email) => {
@@ -40,16 +60,47 @@ const Contact = () => {
       <form className='contactForm'>
         <div className='messageContainer'>
           <h3>Ma demande</h3>
-          <div className='messageContent'>
+
+          <div className='messageElement objectContent'>
+            <h4>
+              Objet du message <span style={{ color: 'red' }}>*</span>
+            </h4>
+            <input type='text' onChange={e => handleObject(e.target.value)} />
+          </div>
+
+          <div className='messageElement messageContent'>
             <h4>
               Contenu du message <span style={{ color: 'red' }}>*</span>
             </h4>
             <textarea onChange={e => handleMessage(e.target.value)}></textarea>
           </div>
         </div>
-        <div className='emailContainer'>
+        <div className='infoContainer'>
           <h3>Mes coordonnées</h3>
-          <div className='emailContent'>
+
+          <div className='infoElement lastNameContent'>
+            <h4>
+              Nom <span style={{ color: 'red' }}>*</span>
+            </h4>
+            <input
+              type='text'
+              placeholder='Dupont'
+              onChange={e => handleLastName(e.target.value)}
+            />
+          </div>
+
+          <div className='infoElement firstNameContent'>
+            <h4>
+              Prénom <span style={{ color: 'red' }}>*</span>
+            </h4>
+            <input
+              type='text'
+              placeholder='Nicolas'
+              onChange={e => handleFirstName(e.target.value)}
+            />
+          </div>
+
+          <div className='infoElement emailContent'>
             <h4>
               E-mail <span style={{ color: 'red' }}>*</span>
             </h4>
@@ -59,7 +110,13 @@ const Contact = () => {
               onChange={e => handleEmail(e.target.value)}
             />
           </div>
+
+          <div className='infoElement phoneContent'>
+            <h4>Téléphone</h4>
+            <input type='phone' onChange={e => handlePhone(e.target.value)} />
+          </div>
         </div>
+
         <input
           type='submit'
           className='submitButton'
