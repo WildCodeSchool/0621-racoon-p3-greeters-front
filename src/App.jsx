dbf0aae2434fb75e4e3d2ed3b16d59fd1225af26
Shawn <shawn17@live.fr>
2021-09-23 10:22:21 +0200

    fixing the map icon, unable the zoom on scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Contact from './components/Contact/Contact'
+import Contact from './screens/Contact/Contact'
 import Navbar from './components/Navbar/Navbar'
 import Home from './screens/Home/Home'
 import Greeter from './screens/Greeter/Greeter'
