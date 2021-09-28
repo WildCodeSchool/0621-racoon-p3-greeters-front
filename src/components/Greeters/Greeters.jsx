83f01a6b73990aaa78d8fbc18c648b5e3082abee
RomainM81 <dcromain112@gmail.com>
2021-09-28 17:18:43 +0100

    linked front and back on greeters

--- src/components/Greeters/Greeters.jsx
+++ src/components/Greeters/Greeters.jsx
@@ -1,18 +1,31 @@
-import { GreetersData } from '../../data'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import GreeterCard from '../GreeterCard/GreeterCard'
 
 import './Greeters.css'
 
 const Greeters = () => {
+  const [greeters, setGreeters] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const resData = await axios.get('http://localhost:3000/person/limit')
+      setGreeters(resData.data.result)
+    }
+    getData()
+  }, [])
+
+  console.log(greeters)
+
   return (
     <>
       <h2 className='greeters-title'>Les Greeters</h2>
 
       <div className='greeters-container'>
-        {GreetersData.map((g, index) => (
-          <GreeterCard key={index} {...g} />
-        ))}
+        {greeters
+          ? greeters.map((g, index) => <GreeterCard key={index} {...g} />)
+          : null}
       </div>
       <button className='greeters-btn'>Tout Voir</button>
     </>
